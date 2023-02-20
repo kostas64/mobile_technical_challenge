83a02826f327c8e017881960a87ed7fb186d5fc5
Konstantinos Efkarpidis <kefkarpidis@deloitte.gr>
2023-02-20 13:17:01 +0200

    fixed uri for android

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,8 +1,12 @@
+import { Platform } from "react-native";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-  uri: "http://127.0.0.1:3000/graphql",
+  uri:
+    Platform.OS === "ios"
+      ? "http://127.0.0.1:3000/graphql"
+      : "http://10.0.2.2:3000/graphql",
   cache,
 });
